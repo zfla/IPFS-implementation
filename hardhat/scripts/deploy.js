cf6920bb25b47e4685b3e620e8a03ebec7f46422
Zain Fox-Latif <zainfox-latif@zains-air.home>
2022-08-03 22:20:16 +0100

    first commit

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,18 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  const metadataURL = "ipfs://Qmbygo38DWF1V8GttM1zy89KzyZTPU2FLUzQtiDvB7q6i5/";
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+  await deployedLW3PunksContract.deployed();
+  console.log("Deployed LW3Punks contract address:", deployedLW3PunksContract.address);
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
